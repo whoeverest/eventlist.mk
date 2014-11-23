c1f725622f2eb73fbea50e8779883e9e9d2262a7
Andrej T <andrejtrajchevski@gmail.com>
2014-11-23 16:18:25 +0100

    remove canceled events on every refresh

--- scripts/remove-canceled.js
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/node
-
-var DB = require('../lib/db-service');
-
-DB.events.removeCanceled()
-    .then(function() { process.exit(0); })
-    .catch(function() { process.exit(1); });
