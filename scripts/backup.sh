1c3b3cfd0094570148efc1384ea1a991394f0dd6
Andrej T <andrejtrajchevski@gmail.com>
2014-11-25 01:03:49 +0100

    iso8601 formating on backup dates

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/local/bin/rethinkdb-dump -c localhost:28015 -f /home/whoeverest/eventlist-backups/eventlist_$(date +"%Y-%M-%d_%H:%M")_tar.gz
+/usr/local/bin/rethinkdb-dump -c localhost:28015 -f /home/whoeverest/eventlist-backups/eventlist_$(date --iso-8601="minutes")_tar.gz
