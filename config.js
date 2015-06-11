65a576c707da81302b37468cec56537e231cddad
Andrej T <andrejtrajchevski@gmail.com>
2015-06-11 14:19:57 +0200

    removed config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	fb: {
-		clientID: '1471063303131642',
-		clientSecret: 'c5197e02fc6fad6537162806cf161f0b'
-	},
-	express: {
-		secret: 'OO*gh84f2oiyfu28g24oiugfvy2498gy2498g2y4iugfev'
-	}
-}
